44d7d9023f81112bf72c7f504411fd6007a6af49
SirLalaPyon <76900263+SirLalaPyon@users.noreply.github.com>
2021-06-07 12:18:56 +0800

    Update

--- Source/DankPyon_MedievalOverhaul/DankPyon_MedievalOverhaul/DankPyon_DamagerWorker_ExplosionNoFlash.cs
+++ Source/DankPyon_MedievalOverhaul/DankPyon_MedievalOverhaul/DankPyon_DamagerWorker_ExplosionNoFlash.cs
@@ -6,10 +6,10 @@ using System.Text;
 using System.Threading.Tasks;
 using Verse;
 
-namespace DankPyon_MedievalOverhaul
+namespace DankPyon
 {
-	// use with DankPyon_MedievalOverhaul.DamagerWorker_ExplosionNoFlash
-	public class DamagerWorker_ExplosionNoFlash : DamageWorker
+	// use with DankPyon.DamagerWorker_ExplosionNoFlash
+	public class DamagerWorker_ExplosionNoFlash : DamageWorker_AddInjury
     {
         public override void ExplosionStart(Explosion explosion, List<IntVec3> cellsToAffect)
 		{
@@ -17,12 +17,12 @@ namespace DankPyon_MedievalOverhaul
 			{
 				GenTemperature.PushHeat(explosion.Position, explosion.Map, this.def.explosionHeatEnergyPerCell * (float)cellsToAffect.Count);
 			}
-		//	MoteMaker.MakeStaticMote(explosion.Position, explosion.Map, ThingDefOf.Mote_ExplosionFlash, explosion.radius * 6f);
+		/*//	MoteMaker.MakeStaticMote(explosion.Position, explosion.Map, ThingDefOf.Mote_ExplosionFlash, explosion.radius * 6f);
 			if (explosion.Map == Find.CurrentMap)
 			{
 				float magnitude = (explosion.Position.ToVector3Shifted() - Find.Camera.transform.position).magnitude;
 				Find.CameraDriver.shaker.DoShake(4f * explosion.radius / magnitude);
-			}
+			}*/
 			this.ExplosionVisualEffectCenter(explosion);
 		}
     }

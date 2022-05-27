215bdffc8fe8b1aae85af9a7bfca4feaf9b45e29
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-05-27 13:07:48 +1200

    CompProperties_PawnSpawnerOnDestroy

---
+++ Source/CompProperties_PawnSpawnerOnDestroy.cs
@@ -0,0 +1,38 @@
+﻿using RimWorld;
+using Verse;
+using System;
+using System.Collections.Generic;
+
+namespace MedievalOverhaul
+{
+    class CompProperties_PawnSpawnerOnDestroy : CompProperties
+    {
+        public CompProperties_PawnSpawnerOnDestroy()
+        {
+            this.compClass = typeof(Comp_PawnSpawnerOnDestroy);
+        }
+
+        public PawnKindDef kindToSpawn = null;
+        public bool spawnAsPlayerFaction = true;
+        public FactionDef faction;
+        public MentalStateDef mentalStateDef;
+        public IntRange numberToSpawn;
+        public SoundDef soundDef;
+        public float chance = 1f;
+
+        /* For sending a letter when spawning happens */
+        public bool sendLetter = true;
+        public string letterLabel;
+        public string letterDescription;
+        public LetterDef letterDef;
+
+        /* Disables spawning if destroyed by specific damage types
+         * You probably don't want angry tree people spawning from trees that die of old age
+         */
+        public bool notRot = true;
+        public bool notFlame = true;
+        public bool notDeterioration = true;
+        /* disables spawning if there is currently a toxic fallout active */
+        public bool notToxicFallout = true;
+    }
+}

f55868b408696ec74fdccc850abb963247f713bd
Taranchuk <nfhfyxer@gmail.com>
2022-06-08 20:54:41 +0300

    wip

--- Source/DefOfs/DankPyonDefOf.cs
+++ Source/DefOfs/DankPyonDefOf.cs
@@ -11,6 +11,11 @@ namespace DankPyon
         public static ThingDef DankPyon_Artillery_Boulder;
         public static TerrainDef DankPyon_PlowedSoil;
         public static DamageDef DankPyon_SchratCollapse;
+        public static ThingDef DankPyon_Hide_HideGeneric;
+        public static BodyPartGroupDef HeadAttackTool;
+
+        public static ThingDef DankPyon_Fat;
+        public static ThingDef DankPyon_Bone;
     }
 
 }

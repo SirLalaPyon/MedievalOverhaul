a62259924b45e0b1d5ec0de446acdf107959a849
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-11-23 09:09:32 +1300

    CompGenericHide PostSplitOff fix
    Adding in an override for PostSplitOff, ensuring the thing that was split retains the stacks information.

--- Source/Harmony/CompGenericHide.cs
+++ Source/Harmony/CompGenericHide.cs
@@ -17,6 +17,29 @@ namespace DankPyon
             }
             return base.AllowStackWith(other);
         }
+
+        /// <summary>
+        /// Ensures information inside variables is retained after splitting off from a stack
+        /// </summary>
+        /// <param name="piece">
+        /// Is the thing that was split off
+        /// </param>
+        public override void PostSplitOff(Thing piece)
+        {
+            base.PostSplitOff(piece);
+            CompGenericHide otherComp = piece.TryGetComp<CompGenericHide>();
+            if (otherComp != null)
+            {
+                otherComp.pawnSource = this.pawnSource;
+                otherComp.leatherAmount = this.leatherAmount;
+                otherComp.marketValue = this.marketValue;
+                if (piece is HideGeneric hideGeneric)
+                {
+                    hideGeneric.drawColorOverride = this.pawnSource.race.leatherDef.graphicData.color;
+                }
+            }
+        }
+
         public override void PostExposeData()
         {
             base.PostExposeData();

49f07bf3bf0a3abced1c4c06af01be9c3c66c883
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2024-03-28 23:28:34 -0600

    Early out FindBestFuel

--- 1.5/Source/ESCP_FuelExtension/HarmonyPatches/FindBestFuel_Prefix.cs
+++ 1.5/Source/ESCP_FuelExtension/HarmonyPatches/FindBestFuel_Prefix.cs
@@ -3,6 +3,7 @@ using RimWorld;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Verse;
@@ -27,12 +28,29 @@ namespace ESCP_FuelExtension
             if (comp1 != null && comp2 != null)
             {
                 ThingFilter filter = refuelable.TryGetComp<CompStoreFuelThing>().AllowedFuelFilter;
-                Predicate<Thing> validator = (Thing x) => !x.IsForbidden(pawn) && pawn.CanReserve(x, 1, -1, null, false) && filter.Allows(x);
-                IEnumerable<Thing> searchSet = pawn.Map.listerThings.ThingsMatchingFilter(filter);
-                __result = GenClosest.ClosestThingReachable(pawn.Position, pawn.Map, ThingRequest.ForGroup(ThingRequestGroup.HaulableEver), PathEndMode.ClosestTouch, TraverseParms.For(pawn, Danger.Some, TraverseMode.ByPawn), 9999f, validator, searchSet, 0, -1, false, RegionType.Set_Passable, false);
+                if (FilterItemExists(filter, pawn))
+                {
+                    Predicate<Thing> validator = (Thing x) => !x.IsForbidden(pawn) && pawn.CanReserve(x, 1, -1, null, false) && filter.Allows(x);
+                    IEnumerable<Thing> searchSet = pawn.Map.listerThings.ThingsMatchingFilter(filter);
+                    __result = GenClosest.ClosestThingReachable(pawn.Position, pawn.Map, ThingRequest.ForGroup(ThingRequestGroup.HaulableEver), PathEndMode.ClosestTouch, TraverseParms.For(pawn, Danger.Some, TraverseMode.ByPawn), 9999f, validator, searchSet, 0, -1, false, RegionType.Set_Passable, false);
+                }
                 return false;
             }
             return true;
+        }
+        public static bool FilterItemExists(ThingFilter filter, Pawn pawn)
+        {
+            bool Checked = false;
+            foreach (var def in filter.AllowedThingDefs)
+            {
+                List<Thing> thingsOfDef = pawn.Map.listerThings.ThingsOfDef(def);
+                if (thingsOfDef.Count > 0)
+                {
+                    Checked = true;
+                    break;
+                }
+            }
+            return Checked;
         }
     }
 }

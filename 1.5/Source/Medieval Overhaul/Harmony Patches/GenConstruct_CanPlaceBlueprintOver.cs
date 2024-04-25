1d52f0738797678cb93956f24a4582736c132b6d
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2024-04-25 19:51:46 +0200

    Harmony patch for golem rocks spawn

--- 1.5/Source/Medieval Overhaul/Harmony Patches/GenConstruct_CanPlaceBlueprintOver.cs
+++ 1.5/Source/Medieval Overhaul/Harmony Patches/GenConstruct_CanPlaceBlueprintOver.cs
@@ -1,5 +1,4 @@
 ﻿using HarmonyLib;
-using MedievalOverhaul;
 using RimWorld;
 using Verse;
 
---
+++ 1.5/Source/Medieval Overhaul/Harmony Patches/ThingSetMaker_Meteorite_Reset.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace MedievalOverhaul.Patches
+{
+    [HarmonyPatch(typeof(ThingSetMaker_Meteorite), "Reset")]
+    public static class ThingSetMaker_Meteorite_Reset
+    {
+        public static void Postfix()
+        {
+            List<ThingDef> thingDefs = new List<ThingDef>();
+            for (int i = 0; i < ThingSetMaker_Meteorite.nonSmoothedMineables.Count; i++)
+            {
+                ThingDef thing = ThingSetMaker_Meteorite.nonSmoothedMineables[i];
+                if (thing.thingClass.Name.Equals("Mineable_CompSpawnerDestroy"))
+                {
+                    thingDefs.Add(thing);
+                }
+            }
+            for (int t = 0; t < thingDefs.Count; t++)
+            {
+                if (ThingSetMaker_Meteorite.nonSmoothedMineables.Contains(thingDefs[t]))
+                {
+                    ThingSetMaker_Meteorite.nonSmoothedMineables.Remove(thingDefs[t]);
+                }
+            }
+            thingDefs.Clear();
+        }
+    }
+}

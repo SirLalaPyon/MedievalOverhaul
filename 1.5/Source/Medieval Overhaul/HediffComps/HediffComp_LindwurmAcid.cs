391192d41c8857d6754edd15c0666c4d3d2edb64
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2024-04-26 20:43:02 +0200

    Lindwurm Acid Bite
    * Damages apparel
    * ModExtension to prevent damage from acid on certain apparel
    * Hediff Giver Set to prevent old age on Lindwurm

---
+++ 1.5/Source/Medieval Overhaul/HediffComps/HediffComp_LindwurmAcid.cs
@@ -0,0 +1,43 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace MedievalOverhaul
+{
+    public class HediffComp_LindwurmAcid : HediffComp
+    {
+        public HediffCompProperties_LindwurmAcid Props
+        {
+            get
+            {
+                return (HediffCompProperties_LindwurmAcid)this.props;
+            }
+        }
+
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            Pawn pawn = this.Pawn;
+            if (this.Pawn.IsHashIntervalTick(Props.tickInterval))
+            {
+                List<Apparel> apparelList = pawn.apparel.WornApparel;
+                for (int i = 0; i < apparelList.Count; i++)
+                {
+                    Thing apparel = apparelList[i];
+                    if (apparel != null && apparel.def.GetModExtension<ApparelExtension_ImmuneLindwurmAcid>() == null)
+                    {
+                        if (apparel.HitPoints <= Props.apparelDamagePerInterval)
+                        {
+                            apparel.HitPoints = 0;
+                            apparel.Destroy(DestroyMode.Vanish);
+                           // Find.LetterStack.ReceiveLetter(this.Props.letterLabelOnDisappear.Formatted(base.Pawn.Named("PAWN")), this.Props.letterTextOnDisappear.Formatted(base.Pawn.Named("PAWN")), LetterDefOf.PositiveEvent, base.Pawn, null, null, null, null, 0, true);
+                        }
+                        else
+                            apparel.HitPoints -= Props.apparelDamagePerInterval;
+                    }
+
+                }
+            }
+            
+        }
+    }
+}
